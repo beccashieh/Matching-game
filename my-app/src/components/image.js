3a905e92d2cec4412522efcda1d0c059dc89a4a2
BShieh <rebeccasshieh@gmail.com>
2019-07-08 14:19:22 -0400

    wip: removed click event from image comp

--- my-app/src/components/image.js
+++ my-app/src/components/image.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Image(props) {
     return (
-        <div className="card" onClick={this.handleChange}>
+        <div className="card">
           <div className="img-container">
             <img src={props.link} alt={props.id} />
             <div className="card-body"></div>
