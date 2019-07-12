3fe6682031b79b07c9d61f1d9090395010f1c1f3
BShieh <rebeccasshieh@gmail.com>
2019-07-12 15:02:40 -0400

    Updates

---
+++ precache-manifest.afefaf8be5d3178f685cec7f5387b636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e18b4077f040063c9c1c6bf5b70a15",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "264859aee017a55b54d3",
+    "url": "/memory-game/static/css/main.c564af4f.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/memory-game/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "264859aee017a55b54d3",
+    "url": "/memory-game/static/js/main.cd8015bd.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
